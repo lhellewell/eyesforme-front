e7e29b1ad5ed9028e0469832cfa3be9e3c3cb665
Lewis Hellewell <imlewishellewell@gmail.com>
2023-02-27 13:11:43 +0800

    style: inputs now use direct html elements

--- src/Components/Inputs.tsx
+++ src/Components/Inputs.tsx
@@ -22,25 +22,20 @@ const Inputs : React.FC<any> = ({onFileUpload, setTask, setInput}) => {
 	}	
 
 	return (
-		<form onSubmit={(event) => event.preventDefault()} className="flex flex-col gap-4 font-medium text-3xl">
-			<div id="select">
-			<div className="mb-1 block">
-				<Label
-				htmlFor="task"
-				value="Please Select Task"
-				/>
-			</div>
-			<Select id="task" onChange={onChangeTask} className="" required={true}>
-				<option>
-				Image Captioning
-				</option>
-				<option>
-				Image Question Answering
-				</option>
-				<option>
-				Image Text Matching
-				</option>
-			</Select >
+		<form onSubmit={(event) => event.preventDefault()} className="flex flex-col gap-4 font-medium">
+			<div id="select" className="">
+				<div className="mb-1 block">
+					<Label
+					htmlFor="task"
+					value="Please Select Task"
+					className="text-base"
+					/>
+				</div>
+				<select id="task" onChange={onChangeTask} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+					<option defaultValue={"true"} value="Image Captioning">Image Captioning</option>
+					<option value="Image Question Answering">Image Question Answering</option>
+					<option value="Image Text Matching">Image Text Matching</option>
+				</select>
 			</div>
 			<div>
 				<div className="mb-2 block">
